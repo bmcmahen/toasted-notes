8cb2575a42f359bac6abad430a18b2d682381fe9
Ben McMahen <bmcmahen@Bens-MacBook-Pro.local>
2019-02-20 14:47:59 -0800

    use types field in package.json

--- src/Alert/index.ts
+++ src/Alert/index.ts
@@ -1,7 +1,13 @@
 import Toaster from "./Toast";
+import { PositionsType, MessageProp } from "../../es/Alert/Message";
+import { MessageOptionalOptions } from "../../lib/Alert/ToastManager";
 
 const toaster = new Toaster();
 
 export { default as Position } from "./Positions";
+
+export type ToastPositions = PositionsType;
+export type ToastMessage = MessageProp;
+export type ToastOptions = MessageOptionalOptions;
 
 export default toaster;
